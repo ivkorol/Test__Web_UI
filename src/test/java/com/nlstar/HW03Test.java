cf2f912e580ea09fe652284615087fab4a1c5564
irina korol <irinakorol@gmail.com>
2022-01-30 19:18:07 +0300

    Hw05

--- src/test/java/com/nlstar/HW03Test.java
+++ src/test/java/com/nlstar/HW03Test.java
@@ -1,10 +1,8 @@
 package com.nlstar;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.interactions.Actions;
@@ -29,11 +27,27 @@ public class HW03Test {
         webDriverWait = new WebDriverWait(driver, Duration.ofSeconds(5));
         actions = new Actions(driver);
         driver.get(NLSTAR_URL);
+        driver.manage().window().maximize();
 
     }
 
     @Test
+    void hoverLoginTest() {
+        actions.moveToElement(driver.findElement(By.xpath("//a[@href='/ru/store/login/']")))
+                .build()
+                .perform();
+        driver.findElement(By.xpath("//div[@class='authorization authorization_login']//a[@href='/ru/store/login/']")).click();
+        Assertions.assertTrue(driver.getCurrentUrl().contains("Войти"));
+    }
 
+    @Test
+    void hoverCartTest() {
+        actions.moveToElement(driver.findElement(By.xpath("//button[.='В корзину']")))
+                .build()
+                .perform();
+        driver.findElement(By.xpath("//a[@href='/ru/store/cart']")).click();
+        Assertions.assertTrue(driver.getCurrentUrl().contains("Оформить заказ"));
+    }
 
 
     @AfterEach
